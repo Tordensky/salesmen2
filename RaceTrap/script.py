a0672749c871a98ee3021dc25b83869dcd56850d
Simon Nistad <simon.nistad@hotmail.com>
2012-10-16 15:32:23 +0200

    Created a test script

--- RaceTrap/script.py
+++ RaceTrap/script.py
@@ -3,130 +3,173 @@ import os
 import StringIO
 import time
 
-MORE_NODES = True
-#nodes = ['0-3', '1-0', '1-1', '1-3', '2-0', '2-2', '2-3', '3-2', '4-3', '5-1', '6-1', '6-2', '6-3', '7-1', '7-2', '7-3', '8-0', '8-1', '8-2', '8-3', '9-0']
-nodes = ['9-0']
-runningNodes = ['0-0', '0-1', '0-3', '1-0', '1-1', '1-3', '2-0', '2-2', '2-3', '3-2', '4-3', '5-1', '6-1', '6-2', '6-3', '7-1', '7-2', '7-3', '8-0', '8-1', '8-2', '8-3']
 
-def change_size(filename, size):
-    f = open((filename+".c"), "r+")
+def change_number_of_cities(newNum):
+	try:
+		f = open("route.dat", "r+")
+		
+		content = f.readlines()
+		
+		f.seek(0)
+		
+		f.write(str(newNum)+"\n")
+		
+		content.pop(0)
+		
+		for line in content:
+        	
+        		f.write(str(line))
+		
+		f.close()
+	except:
+		print "Error while trying to change number of cities"
 
-    content = f.readlines()
-    
-    f.seek(0)
-    
-    f.write("#define WIDTH " + str(size) + "\n")
-    f.write("#define HEIGHT " + str(size) + "\n")
-    
-    content.pop(0)
-    content.pop(0)
-    for line in content:
+MIN_NUM_CITIES = 10
+MAX_NUM_CITIES = 11
+
+MIN_NUM_WORKERS = 1
+MAX_NUM_WORKERS = 8
+
+NUM_RUNS = 5
+
+FILE_NAMES = ["RaceTrap", "working"]
+
+f = open("results.txt", "w+")
+
+f.write("FILENAME, NUM_CITIES, NUM_WORKERS, RUNTIME\n")
+
+# Running different versions
+for file in FILE_NAMES:
+	
+	
+	
+	
+	# Running different number of cities
+	for curr_num_cities in range(MIN_NUM_CITIES, MAX_NUM_CITIES+1):
+		
+		change_number_of_cities(curr_num_cities)
+		
+		# Running different number of workers
+		for curr_num_workers in range(MIN_NUM_WORKERS, MAX_NUM_WORKERS):
+
+		
+		
+			
+			avg = []
+			
+			print "Filename", file, "\tCities: ", curr_num_cities, "\tWorkers ", curr_num_workers
+			for run in range(NUM_RUNS):
+				
+				
+				output = os.popen("./" + file + " -cilk_set_worker_count=" + str(curr_num_workers)).read()
+				output = StringIO.StringIO(output)
+				
+				x =  output.readline().split(" ")
+				#print x
+				
+				sumRunning = 0
+				
+				for count in range(0, len(x)):
+					if x[count] == "min.":
+						
+						sumRunning += int(x[count - 1]) * 60000
+			
+					if x[count] == "seconds":
+						sumRunning += int(x[count -1])*1000
+			
+					if x[count] == "ms":
+						ms =  int(x[count-1])
+						sumRunning += ms   
+						
+				avg.append(sumRunning)		
+				print "RUNTIME", sumRunning
+			
+			avg_runtime = sum(avg) / len(avg)
+			print "AVG: ", avg_runtime
+			results = "%s, %d, %d, %d \n" % (file, curr_num_cities, curr_num_workers, avg_runtime)
+			f.write(results)
+f.close()
+		
+
+#def make(filename):
+    	#output = os.popen(('make %s' % (file_name))).read()
+	
+	
+
+
+
+##a = os.popen('lamboot -v hosts.lam').read()
+##p.wait()
+#timelist = []
+#tmplist = []
+
+#sizes = [16, 32, 64, 128, 256, 512, 1024, 2048]
+##sizes = [8192]
+
+##files = ["dynamic", "master"]
+#files = ["RoadMap"]
+#runs = 1
+
+
+
+
+##changeNodeCount()
+
+#time.sleep(2)
+
+#print "Execute test"
+
+#f =  open("seqTest.cvs", "w+")
+#columns = "nodes, filename, average, numruns, size\n"
+#f.write(columns)
+
+
+##while (MORE_NODES):
+    ##f.write(("\n\nRUNNING ON %d NODES\n"%(len(runningNodes))))
+    ##os.popen("lamboot -v hosts.lam")
+#for file_name in files:
+    #for size in sizes:
+        #change_size(file_name, size)
+        #make(file_name)
         
-        f.write(str(line))
-    
-    f.close()
-    
-def changeNodeCount():
-    output = os.popen("lamhalt").read()
-       
-    if len(nodes) != 0:
-        runningNodes.append(nodes.pop())
+        #tmplist = []
         
-        lamfile  = open("hosts.lam", 'w')
-        for node in runningNodes:
-            lamfile.write(("compute-%s\n") % (node))
+        #for i in range (0, runs):
+            ##a = os.popen(('mpirun C -ssi rpi tcp ./%s' % (file_name))).read()
+            #a = os.popen(('./%s' % (file_name))).read()
             
-        lamfile.close()
-        
-        
-        
-    else:
-        MORE_NODES = False
-
+            
+            #a = StringIO.StringIO(a)
+            
+            #sumRunning = 0
+            #for line in a.readlines():
+                    ## if line.count("Time taken:") == 1:
     
-
-
-def make(filename):
-    output = os.popen(('make %s' % (file_name))).read()
-    #print output
-
-#starting the computers
-
-
-
-
-#a = os.popen('lamboot -v hosts.lam').read()
-#p.wait()
-timelist = []
-tmplist = []
-
-sizes = [16, 32, 64, 128, 256, 512, 1024, 2048]
-#sizes = [8192]
-
-#files = ["dynamic", "master"]
-files = ["RoadMap"]
-runs = 1
-
-
-
-
-#changeNodeCount()
-
-time.sleep(2)
-
-print "Execute test"
-
-f =  open("seqTest.cvs", "w+")
-columns = "nodes, filename, average, numruns, size\n"
-f.write(columns)
-
-
-#while (MORE_NODES):
-    #f.write(("\n\nRUNNING ON %d NODES\n"%(len(runningNodes))))
-    #os.popen("lamboot -v hosts.lam")
-for file_name in files:
-    for size in sizes:
-        change_size(file_name, size)
-        make(file_name)
-        
-        tmplist = []
-        
-        for i in range (0, runs):
-            #a = os.popen(('mpirun C -ssi rpi tcp ./%s' % (file_name))).read()
-            a = os.popen(('./%s' % (file_name))).read()
-            
-            
-            a = StringIO.StringIO(a)
-            
-            sumRunning = 0
-            for line in a.readlines():
-                    # if line.count("Time taken:") == 1:
-    
-                x = line.split(" ")
+                #x = line.split(" ")
                 
-                for count in range(0, len(x)):
-                    if x[count] == "min.":
+                #for count in range(0, len(x)):
+                    #if x[count] == "min.":
                         
-                            sumRunning += int(x[count - 1]) * 60000
+                            #sumRunning += int(x[count - 1]) * 60000
 
-                    if x[count] == "seconds":
-                        sumRunning += int(x[count -1])*1000
+                    #if x[count] == "seconds":
+                        #sumRunning += int(x[count -1])*1000
 
-                    if x[count] == "ms":
-                        ms =  int(x[count-1])
-                        sumRunning += ms   
+                    #if x[count] == "ms":
+                        #ms =  int(x[count-1])
+                        #sumRunning += ms   
 
-            tmplist.append(sumRunning)    
+            #tmplist.append(sumRunning)    
                         
         
-        result =  "%s, %s, %d, %d, %d X %d\n" % (len(runningNodes), file_name, (sum(tmplist) / len(tmplist)), runs, size, size)
-        print result
-        f.write(result)
-    #changeNodeCount()
+        #result =  "%s, %s, %d, %d, %d X %d\n" % (len(runningNodes), file_name, (sum(tmplist) / len(tmplist)), runs, size, size)
+        #print result
+        #f.write(result)
+    ##changeNodeCount()
 
 
-#output = os.popen("lamhalt").read()
-f.close
+##output = os.popen("lamhalt").read()
+#f.close
 
 
 
